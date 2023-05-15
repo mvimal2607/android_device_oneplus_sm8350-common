62c9dd879ca54698b8a99a693caa0b5c296b8ce9
pjgowtham <pjgowtham@gmail.com>
2023-05-15 14:11:46 +0530

    sm8350-common: Run oplusSensor hal as root user
    For the hal not to be denied access to oplusreserve partitions
    
    Change-Id: Ic1da178aa0fd61c485135650044d76c1f0cc040d

--- extract-files.py
+++ extract-files.py
@@ -47,6 +47,8 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'odm/bin/hw/vendor.pixelworks.hardware.display.iris-service': blob_fixup()
         .add_needed('libprocessgroup.so'),
+    'odm/etc/init/vendor-oplus-hardware-oplusSensor-V1-service.rc': blob_fixup()
+        .regex_replace('user system', 'user root'),
     ('odm/lib64/mediadrm/libwvdrmengine.so', 'odm/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
     'product/app/PowerOffAlarm/PowerOffAlarm.apk': blob_fixup()
