b8d62487d697d310ba0c5af9ea51d62c961b3910
pjgowtham <pjgowtham@gmail.com>
2024-01-14 19:11:27 +0530

    sm8350-common: Switch to OSS HWC
    * Remove oplus cwb components
    * Remove libsnapdragoncolor-pxlw.so library because it is not a part of snapdragon_color_libs_config.xml anymore
    * A lot of the realme devices either just support soft-iris OR no pixelworks services at all.
    
    Change-Id: I3c291e780a607b68cce0e80d0a60f5276f60e1a0

--- common.mk
+++ common.mk
@@ -113,12 +113,12 @@ PRODUCT_PACKAGES += \
     init.qti.display_boot.rc \
     init.qti.display_boot.sh \
     vendor.qti.hardware.display.allocator-service \
-    vendor.qti.hardware.display.composer-service.rc \
-    vendor.qti.hardware.display.composer-service.xml \
+    vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.memtrack-service
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
 # Doze
 PRODUCT_PACKAGES += \
