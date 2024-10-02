0d890028b845ab0a774495d6db530da0798d072b
mvimal2607 <mvimal2607@gmail.com>
2024-10-02 16:58:00 +0530

    sm8350-common: Compile with r487747c clang version
    Signed-off-by: mvimal2607 <mvimal2607@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,6 +112,7 @@ BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8350
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
 TARGET_KERNEL_NO_GCC := true
+TARGET_KERNEL_CLANG_VERSION := r487747c
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
