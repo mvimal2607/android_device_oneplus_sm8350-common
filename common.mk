0f762bb420f8d51696228527db6955ad470ab25b
pjgowtham <pjgowtham@gmail.com>
2024-10-05 19:08:53 +0530

    sm8350-common: Avoid building oplus doze
    AOSP doze already covers most of the features.
    
    What is not supported by AOSP is
    - Using pickup sensor for doze pulse
    - Toggling quick pickup sensor irrespective of pickup gesture
    
    But these are probably not the features that we'd want to expose.
    
    Change-Id: I8655baceb4be3e3a68613486da4ac4c0c5ff93d6

--- common.mk
+++ common.mk
@@ -124,10 +124,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
-# Doze
-PRODUCT_PACKAGES += \
-    OplusDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
