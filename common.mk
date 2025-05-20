767018db6dd08e72222d0510555ad8fdc1311068
mvimal2607 <mvimal2607@gmail.com>
2025-05-20 12:46:35 +0000

    sm8350-common: Don't build lineage health
    Signed-off-by: mvimal2607 <mvimal2607@gmail.com>

--- common.mk
+++ common.mk
@@ -202,10 +202,6 @@ $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_oplus)
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.oplus
