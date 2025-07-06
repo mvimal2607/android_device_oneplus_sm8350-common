2a1cc964351e1d82d5306639e4f7cea7d2df2e48
LuK1337 <priv.luk@gmail.com>
2025-07-06 12:54:55 +0200

    sm8350-common: apktool_patch() no longer takes custom arguments
    Change-Id: Idf85ae2cd0505c34e55f0e6acec2afc085372b78

--- extract-files.py
+++ extract-files.py
@@ -52,7 +52,7 @@ blob_fixups: blob_fixups_user_type = {
     ('odm/lib64/mediadrm/libwvdrmengine.so', 'odm/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
     'product/app/PowerOffAlarm/PowerOffAlarm.apk': blob_fixup()
-        .apktool_patch('blob-patches/PowerOffAlarm.patch', '-s'),
+        .apktool_patch('blob-patches/PowerOffAlarm.patch'),
     'system_ext/bin/wfdservice': blob_fixup()
         .add_needed('libwfdservice_shim.so'),
     'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
