06e9cd66a47ed325d93ae36191ef3218343b05ea
chandu078 <chandudyavanapelli03@gmail.com>
2025-11-02 15:59:54 +0000

    sm8350-common: Move manifests to vintf directory
    Change-Id: I2a82e3ae75a1293318efeea0519cfbe64eb4b364

--- common.mk
+++ common.mk
@@ -348,8 +348,8 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/oplus/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
-ODM_MANIFEST_FILES := $(LOCAL_PATH)/manifest_odm.xml
+DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/vintf/manifest.xml
+ODM_MANIFEST_FILES := $(LOCAL_PATH)/vintf/manifest_odm.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
