3f0ff4527f628771af431a4ed7b6a5eefb4600cd
Vimal <mvimal2607@proton.me>
2025-12-25 03:56:37 +0100

    sm8350-common: Kang Poweroff alarm from LA.QSSI.16.0.r1-06700-qssi.0
    Change-Id: I38ed4b7a1cc6b162c97d6cc4f6e252c3b0bc74fe

--- extract-files.py
+++ extract-files.py
@@ -49,8 +49,6 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libprocessgroup.so'),
     ('odm/lib64/mediadrm/libwvdrmengine.so', 'odm/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
-    'product/app/PowerOffAlarm/PowerOffAlarm.apk': blob_fixup()
-        .apktool_patch('blob-patches/PowerOffAlarm.patch'),
     'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
         .regex_replace('/my_product', '/product'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
